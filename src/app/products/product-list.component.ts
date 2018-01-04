c5231c7c43007902cef837aee0983a44ea1aa1bc
Paulo Costa <pajoc527@hotmail.com>
2018-01-04 15:48:11 +0000

    Antes nested components

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -15,7 +15,16 @@ export class ProductListComponent implements OnInit{
     imageWidth: number=50;
     imageMargin: number=2;
     showImage: boolean=false;
-    listFilter: string ='cart';
+    _listFilter: string;
+    get listFilter(): string{
+        return this._listFilter;
+    }
+    set listFilter(value:string){
+        this._listFilter=value;
+        this.filteredProducts= this.listFilter ? this.performFilter (this.listFilter):this.products;
+    }
+
+    filteredProducts: IProduct[];
     products: IProduct[]=[
 
         {
@@ -39,6 +48,18 @@ export class ProductListComponent implements OnInit{
             "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
         }
     ];
+
+    constructor(){
+        this.filteredProducts=this.products;
+        this.listFilter = 'cart';
+    }
+
+    performFilter(filterBy: string): IProduct[]{
+        filterBy=filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy)!== -1);
+    }
+
     //property
     toggleImage():void{
         this.showImage=!this.showImage;
