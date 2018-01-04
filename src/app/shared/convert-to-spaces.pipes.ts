6d1b283b67aa950f9e7cefbbb89ce9091b3d8488
Paulo Costa <pajoc527@hotmail.com>
2018-01-04 16:08:56 +0000

    Testar

--- src/app/shared/convert-to-spaces.pipes.ts
+++ src/app/shared/convert-to-spaces.pipes.ts
@@ -4,6 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
     name: 'convertToSpaces'
 })
 
+
 export class ConvertToSpacesPipe implements PipeTransform{
  
     transform(value: string, character: string): string{
