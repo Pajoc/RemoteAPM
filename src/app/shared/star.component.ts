9341d059dfc895e17511fa44fa72645670998616
Paulo Costa <pajoc527@gmail.com>
2018-01-10 21:47:22 +0000

    Envia e recebe de nested component

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
 import { OnChanges } from "@angular/core/src/metadata/lifecycle_hooks";
+
 
 @Component({
     selector: 'pm-star',
@@ -10,8 +11,12 @@ import { OnChanges } from "@angular/core/src/metadata/lifecycle_hooks";
 export class StarComponent implements OnChanges{
     @Input() rating: number;
     starWidth: number;
-
+    @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
     ngOnChanges(): void{
         this.starWidth=this.rating*86/5;
+    }
+
+    onClick() : void{
+        this.ratingClicked.emit(`The rating ${this.rating} was clicked!`)
     }
 }
