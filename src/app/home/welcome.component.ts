2ba6e8f9956f98207d24f05c921bc7e5bb970006
Paulo Costa <pajoc527@gmail.com>
2018-01-22 21:16:27 +0000

    Menu

--- src/app/home/welcome.component.ts
+++ src/app/home/welcome.component.ts
@@ -2,11 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'pm-root',
-    template: `
-        <div><h1>{{pagetitle}}</h1>
-        <div>My first component</div>
-    </div>
-    `
+    templateUrl: './welcome.component.html'
 
 })
 export class WelcomeComponent{
